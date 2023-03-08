dc814b5bea507a43680aa0d7db9a0640991b3f82
TrilochanprasadBH <104064013+TrilochanprasadBH@users.noreply.github.com>
2023-03-09 00:41:14 +0530

    logic left

---
+++ snake.js
@@ -0,0 +1,14 @@
+//Logic i write must do these thigns - breakdown 
+
+// 1. create a snake eating cube 
+//2. make the cube apprear randomly in playground
+//3. create snake that is made up of cubes that it eats (logic hint)
+//4. set keys up down left right etc 
+//5. game ends on snake hitting end of playground 
+//6. game ends if snake touches itself 
+//7. update score as snake eats cubes 
+//8. set highest score as it happens 
+//9. add somethin extra if i can think of it later
+//10. let it begin !! 
+
+
